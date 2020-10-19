dc7cb5f469a56fd4b6f4517f307d1328b1e95db6
hachiojidev <hachiojidev@protonmail.com>
2020-10-19 15:00:05 +0900

    refactor: Removed ButtonMenuItem styled component

--- src/components/ButtonMenu/ButtonMenuItem.tsx
+++ src/components/ButtonMenu/ButtonMenuItem.tsx
@@ -1,13 +1,10 @@
-import styled from "styled-components";
 import React from "react";
 import Button from "../Button";
 import { sizes } from "../Button/types";
 import { ButtonMenuItemProps } from "./types";
 
-export const StyledButtonMenuItem = styled(Button)``;
-
 const ButtonMenuItem: React.FC<ButtonMenuItemProps> = ({ isActive = false, size = sizes.MD, ...props }) => {
-  return <StyledButtonMenuItem variant={isActive ? "primary" : "secondary"} size={size} {...props} />;
+  return <Button variant={isActive ? "primary" : "secondary"} size={size} {...props} />;
 };
 
 export default ButtonMenuItem;
