7746ef6f697e2093169f29c6c8948c101b0e0e67
hachiojidev <hachiojidev@protonmail.com>
2020-10-20 17:06:49 +0900

    feat(theme): Split out base colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,31 +1,35 @@
 import { Colors } from "../styled.d";
 
-export const lightColors: Colors = {
+export const baseColors = {
+  failure: "#ED4B9E",
   primary: "#1FC7D4",
   primaryBright: "#53DEE9",
   primaryDark: "#0098A1",
   secondary: "#7645D9",
-  tertiary: "#EFF4F5",
   success: "#31D0AA",
-  failure: "#ED4B9E",
-  contrast: "#191326",
-  input: "#F4F2F7",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
   background: "#FAF9FA",
   backgroundDisabled: "#E9EAEB",
+  contrast: "#191326",
+  input: "#F4F2F7",
+  tertiary: "#EFF4F5",
   text: "#452A7A",
   textDisabled: "#BDC2C4",
   textSubtle: "#AEA0D6",
 };
 
 export const darkColors: Colors = {
-  ...lightColors,
-  primaryDark: "#0098A1",
-  tertiary: "#293450",
-  contrast: "#FFFFFF",
-  input: "#151021",
+  ...baseColors,
   background: "#191326",
   backgroundDisabled: "#524B63",
+  contrast: "#FFFFFF",
+  input: "#151021",
+  primaryDark: "#0098A1",
+  tertiary: "#293450",
   text: "#ED4B9E",
-  textSubtle: "#A28BD4",
   textDisabled: "#302B38",
+  textSubtle: "#A28BD4",
 };
