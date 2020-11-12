d52a0f6d03419adb5bee1335965bd85f243600fa
hachiojidev <hachiojidev@protonmail.com>
2020-11-12 17:36:03 +0900

    build: Card folder structure

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,2 @@
-import React from "react";
-import StyledCard from "./StyledCard";
-import { CardProps } from "./types";
-
-const Card: React.FC<CardProps> = ({ ribbon, children, ...props }) => {
-  return (
-    <StyledCard {...props}>
-      {ribbon}
-      {children}
-    </StyledCard>
-  );
-};
-export default Card;
+export { default as Card } from "./Card";
+export { default as CardRibbon } from "./CardRibbon";
