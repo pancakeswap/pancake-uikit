4b077591d479909d37e42beebd3bf0e0c789733b
RichardPK <r.phillips.kerr@gmail.com>
2020-11-18 21:21:29 +0000

    style(progress): Use template string not concat

--- src/components/Progress/StyledProgress.tsx
+++ src/components/Progress/StyledProgress.tsx
@@ -8,7 +8,7 @@ export const Bar = styled.div<BarProps>`
   position: absolute;
   top: 0;
   left: 0;
-  background-color: ${(props) => (props.primary ? props.theme.colors.secondary : props.theme.colors.secondary + `80`)};
+  background-color: ${(props) => (props.primary ? props.theme.colors.secondary : `${props.theme.colors.secondary}80`)};
   border-top-left-radius: 32px;
   border-bottom-left-radius: 32px;
   height: 16px;
