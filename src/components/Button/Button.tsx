7ad916d9ecab125803cfa4d991bfe6adbf9596d5
hachiojidev <hachiojidev@protonmail.com>
2020-12-08 09:55:05 +0900

    chore(button): Button formatting

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import StyledButton from "./StyledButton";
 import { ButtonProps, variants, sizes } from "./types";
 
-const Button: React.FC<ButtonProps> = ({ startIcon, endIcon, children, ...props }) => {
-  return (
-    <StyledButton {...props}>
-      {React.isValidElement(startIcon) &&
-        React.cloneElement(startIcon, {
-          mr: "0.5rem",
-        })}
-      {children}
-      {React.isValidElement(endIcon) &&
-        React.cloneElement(endIcon, {
-          ml: "0.5rem",
-        })}
-    </StyledButton>
-  );
-};
+const Button: React.FC<ButtonProps> = ({ startIcon, endIcon, children, ...props }) => (
+  <StyledButton {...props}>
+    {React.isValidElement(startIcon) &&
+      React.cloneElement(startIcon, {
+        mr: "0.5rem",
+      })}
+    {children}
+    {React.isValidElement(endIcon) &&
+      React.cloneElement(endIcon, {
+        ml: "0.5rem",
+      })}
+  </StyledButton>
+);
 
 Button.defaultProps = {
   variant: variants.PRIMARY,
