8d81acf3aeba2e76a276c5370e6baa979a7b8c42
Guilherme Felipe do Nascimento Reis <guifeliper@gmail.com>
2020-12-19 23:32:52 +0100

    test: Adding Snapshot test for skeleton

---
+++ src/__tests__/components/skeleton.test.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { renderWithTheme } from "../../testHelpers";
+import Skeleton from "../../components/Skeleton/Skeleton";
+
+it("renders correctly", () => {
+  const { asFragment } = renderWithTheme(<Skeleton />);
+  expect(asFragment()).toMatchInlineSnapshot(`
+    <DocumentFragment>
+      <div
+        class="sc-bdfBwQ sc-gsTCUz ljGljN cfCblg"
+      />
+    </DocumentFragment>
+  `);
+});
+
+it("renders correctly avatar", () => {
+  const { asFragment } = renderWithTheme(<Skeleton width={50} height={50} variant="circle"/>);
+  expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <div
+          class="sc-bdfBwQ sc-gsTCUz hbabpS cfCblg"
+          height="50"
+          width="50"
+        />
+      </DocumentFragment>
+  `);
+});
+
+it("renders correctly waves animation", () => {
+  const { asFragment } = renderWithTheme(<Skeleton width={50} height={50} animation="waves"/>);
+  expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <div
+          class="sc-bdfBwQ sc-dlfnbm hSXLgJ jUNhTn"
+          height="50"
+          width="50"
+        />
+      </DocumentFragment>
+  `);
+});
