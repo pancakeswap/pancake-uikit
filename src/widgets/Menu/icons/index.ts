ecaaa1c467a6dd25f60211c86737eb6bfc878b82
JuicyPeachSwap <79715012+JuicyPeachSwap@users.noreply.github.com>
2021-03-03 11:56:59 +0100

    Update index.ts

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -16,3 +16,7 @@ export { default as TelegramIcon } from "./Telegram";
 export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
+export { default as AuditIcon } from "./Audit";
+export { default as PeachIcon } from "./Peach";
+export { default as LayerIcon } from "./Layer";
+export { default as RoadmapIcon } from "./Roadmap";
