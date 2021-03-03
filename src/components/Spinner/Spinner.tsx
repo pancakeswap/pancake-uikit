38a8cbb83a0250a7c04625650c79d2a825127b08
JuicyPeachSwap <79715012+JuicyPeachSwap@users.noreply.github.com>
2021-03-03 11:52:40 +0100

    Update Spinner.tsx

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import PanIcon from "./PanIcon";
-import PancakeIcon from "./PancakeIcon";
 import { SpinnerProps } from "./types";
+import { Svg, SvgProps } from "../Svg";
 
 const rotate = keyframes`
   from {
@@ -13,23 +12,19 @@ const rotate = keyframes`
   }
 `;
 
-const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
-`;
+const SpinnerIcon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 96 96" {...props}>
+      <image width="96" height="96" href="/images/peach/9.png" />
+    </Svg>
+  );
+};
 
 const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
+const RotatingPancakeIcon = styled(SpinnerIcon)`
   position: absolute;
   top: 0;
   left: 0;
@@ -37,18 +32,13 @@ const RotatingPancakeIcon = styled(PancakeIcon)`
   transform: translate3d(0, 0, 0);
 `;
 
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
-  transform: translate3d(0, 0, 0);
-`;
-
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
       <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
     </Container>
   );
 };
 
 export default Spinner;
+
